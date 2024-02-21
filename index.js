e104869e8bf25ef4daa2cb5adfa606aa78607f9d
Nonthakon Tansamai <peearm2003@gmail.com>
2024-02-21 20:00:01 +0700

    'js'

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const router = require('./router/myRouter')
+app.use(express.json());
+ 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router)
+
+app.listen(3399,()=>{
+    console.log('start server in port in 3399')
+})
